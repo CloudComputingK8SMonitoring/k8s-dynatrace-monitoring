8b7d00e9b3e412caa7b1e565c093609e4dbde1f6
Jonas Karg <jonas.karg@hotmail.com>
2025-01-18 19:57:01 +0100

    updated server ip once again

--- multisnake-frontend/src/socket.config.ts
+++ multisnake-frontend/src/socket.config.ts
@@ -2,7 +2,7 @@
 // const SOCKET_PORT = 8000;
 // export const SOCKET_SERVER = `ws://34.149.22.202:${SOCKET_PORT}`;
 
-export const SOCKET_SERVER = `ws://35.190.223.42/ws/game`;
+export const SOCKET_SERVER = `ws://35.187.180.227/ws/game`;
 export const SOCKET_SERVER_RECONNECT_DELAY = 1000;
 export const SOCKET_PLAYER_REGISTERED_PREFIX = 'registered';
 export const SOCKET_PLAYER_DISCONNECTED_PREFIX = 'disconnected';
